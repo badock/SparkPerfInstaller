08bc1a0c2052e540260d8ec34dfbedee4b9b9a6b
Jonathan Pastor <jonathancmoa@gmail.com>
2016-03-17 22:59:48 -0500

    Update install_spark_benchmark.sh

--- install_spark_benchmark.sh
+++ install_spark_benchmark.sh
@@ -26,8 +26,8 @@ sudo service httpd start
 
 
 # Generate a ssh key
-echo -e  'y\n' | ssh-keygen -f .ssh/id_rsa -t rsa -N '' -q
-cat .ssh/id_rsa.pub >> .ssh/authorized_keys
+echo -e  'y\n' | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N '' -q
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 sudo sed -i "s/# Host */Host */g" /etc/ssh/ssh_config
 sudo sed -i "s/#   StrictHostKeyChecking ask/   StrictHostKeyChecking no/g" /etc/ssh/ssh_config
 
